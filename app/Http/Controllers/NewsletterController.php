834a596887a9ac9b1419deaf1d34e27dc089fcef
Mohamedamine <baihmohamedamine@gmail.com>
2024-02-21 11:42:49 +0100

    j'ai pose la base pour la fonctionalite de recherche , en attendant la realisation les 3 crud

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Newsletter;
 use Illuminate\Http\Request;
 
 class NewsletterController extends Controller
 {
-    //
+   public function search(Request $request){
+        $search = $request->search ;
+        $newsletters = Newsletter::where(function($query) use ($search){
+            $query->where('content','LIKE','%'.$search.'%');
+        })
+        ->orWhereHas('categories' , function($query) use ($search){
+            $query->where('name','like','%'.$search.'%');
+        })
+        ->orWherehas('mails' , function($query) use ($search){
+            $query->where('email','like','%'.$search.'%');
+        })->get();
+        return view('newsletter.index' , compact('newsletters'));
+   }
 }
