beb6f6754b09196d43c13b12860c9e502a36e99e
el hrychy niama <niamaelhry@gmail.com>
2024-02-27 16:09:49 +0100

    push

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class MailController extends Controller
 {
-    //
+    // 
+    public function index(){
+        $email =  new Email();
+        $emails = $email->simplePaginate();
+        return view ('email' , compact('emails'));
+    }
+        
+    
+    public function insertEmail(Request $request){
+
+        $request->validate([
+            'name' => 'required',
+        ]);
+        $email = new Email();
+        $email->name = $request->name;
+        $email->save();
+        return redirect('/');
+        }
+
+    //     public function delete_Email(Request $request){
+    //     $id = $request->id;
+    //     $email = Email::find($id);
+    //     $email->delete();
+    //     return redirect('/');
+    // }
 }
